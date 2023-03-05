1b1bed3a374dae6a2b381665bbebaba70ad85689
Abhinav Kumar <kumaraka2000@gmail.com>
2023-03-05 16:11:34 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.Scanner;
+import java.util.function.Predicate;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Predicate<Integer> integerPredicate= i -> (i < 10);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int i = scanner.nextInt();
+		if(integerPredicate.test(i))
+		{
+			System.out.println("The number is less than 10.");
+		}
+		else
+		{
+			System.out.println("The number is greater than 10.");
+		}
+	}
 }
